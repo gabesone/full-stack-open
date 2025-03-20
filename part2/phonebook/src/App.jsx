da4184402e7b9e7d7aafdeff39b3627284ebe15d
gabesone <sena6.gabriel@gmail.com>
2025-03-19 21:04:29 -0300

    Changed the inital data from phonebook to be fetch from the server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
+import phoneService from "./services/service";
 
 export const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [phone, setPhone] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    phoneService
+      .getPersons()
+      .then((initalPhonebook) => setPersons(initalPhonebook));
+  }, []);
 
   function handleNewPerson(e) {
     e.preventDefault();
